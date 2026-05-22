d87f57a942d1ca09e4870dc2efb017b0eb7c3908
mikewithoutthemechanics <236930666+mikewithoutthemechanics@users.noreply.github.com>
2026-05-22 04:59:23 +0200

    Remove Netlify config, Vercel only

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "dist",
-  basePath: process.env.NODE_ENV === "production" ? "/seasoul-wellness" : "",
   images: {
     unoptimized: true,
   },
